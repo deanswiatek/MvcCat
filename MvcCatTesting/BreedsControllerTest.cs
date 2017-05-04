9eede9be95dc285447a93a6c86e1d94d2477d613
LektricSno <mheincode@yahoo.com>
2017-05-04 19:09:20 -0400

    Mostly Decoupled
    Breed Controller data fetches are decoupled with Tests, some
    CatController methods with Tests as well.

---
+++ MvcCatTesting/BreedsControllerTest.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MvcCat.Models;
+using MvcCat.Controllers;
+using System.Web.Mvc;
+
+namespace MvcCatTesting
+{
+    [TestClass]
+    public class BreedsControllerTest
+    {        
+
+        IModelBreed iModelBreed;
+        BreedsController contBreed = new BreedsController();
+        public IModelBreed IModelBreed { get => iModelBreed; set => iModelBreed = value; }
+
+        [TestMethod]
+        public void BreedIndex()
+        {
+            var view = contBreed.Index();
+            Assert.AreNotEqual(null, view);
+        }
+        [TestMethod]
+        public void BreedDetails()
+        {
+            ActionResult breed = contBreed.Details(1);
+            Assert.AreNotEqual(null, breed);
+        }
+
+        [TestMethod]
+        public void BreedCreate()
+        {
+            Breed breed = new Breed
+            {                
+                Name = "Test"
+            };
+
+            ActionResult aResult = contBreed.Create(breed);           
+            Assert.IsInstanceOfType(aResult, typeof(ActionResult));            
+            BreedDel(breed.Id);           
+            
+        }
+        
+        public void BreedDel(int id)
+        {
+            contBreed.Delete(id);
+            contBreed.DeleteConfirmed(id);
+        }
+    }
+}
---
+++ MvcCatTesting/CatsControllerTest.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MvcCat.Models;
+using MvcCat.Controllers;
+using System.Data.SqlClient;
+using System.Data.Entity;
+using System.Web.Mvc;
+
+
+namespace MvcCatTesting
+{
+    [TestClass]
+    public class CatsControllerTest
+    {
+        IModelCat iModelCat;       
+        CatsController contCat = new CatsController();
+
+        public IModelCat IModelCat1 { get => iModelCat; set => iModelCat = value; }
+
+        [TestMethod]
+        public void CatIndex()
+        {
+            //Test that returns an index view, makes sure view is not null                      
+            var view = contCat.Index();
+            Assert.AreNotEqual(null, view);
+        }
+
+        [TestMethod]
+        public void CatDetails()
+        {
+            ActionResult cat = contCat.Details(1);
+            Assert.AreNotEqual(null, cat);
+        }
+        [TestMethod]
+        public void CreateCat()
+        {
+            ActionResult r = contCat.Create();
+            Assert.AreNotEqual(null, r);
+        }
+        //Todo: Test methods for each Query to continue.
+        
+    }
+}
